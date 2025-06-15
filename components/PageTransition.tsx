37e94a0176aab81e70799cd5766330782a37aa6b
a-v-o <72803953+a-v-o@users.noreply.github.com>
2025-06-15 15:06:57 +0100

    Update PageTransition.tsx

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -5,7 +5,7 @@ import { unstable_ViewTransition as ViewTransition } from "react"
 export default function PageTransition() {
   return (
     <ViewTransition name="overlay">
-      <div className="absolute bg-black w-full h-screen -top-[200%] left-0 z-50"></div>
+      <div className="absolute bg-black w-full h-screen -top-[200vh] left-0 z-50"></div>
     </ViewTransition>
   );
 }
